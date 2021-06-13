4cd26dd3320c5e945d1c5062a59e1318ebce0431
quanndm <minhquan1852@gmail.com>
2021-06-13 21:08:44 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+    let gio = document.getElementById('gio');
+    let phut = document.getElementById('phut');
+    let giay = document.getElementById('giay');
+setInterval(()=>{
+    let gio_hien_tai = new Date().getHours();
+    let phut_hien_tai = new Date().getMinutes();
+    let giay_hien_tai = new Date().getSeconds();
+    
+    gio.innerHTML = gio_hien_tai;
+    phut.innerHTML = phut_hien_tai;
+    giay.innerHTML = giay_hien_tai;
+}, 1000);
